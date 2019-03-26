6b52824c1974d1ef7e0c2e066eee73536f4ac57b
Neitron <kirill.zubenko.neitron@gmail.com>
2019-03-26 07:13:54 +0200

    Initial commit

---
+++ Assets/SimplexNoise3d.cs
@@ -0,0 +1,127 @@
+﻿//
+// This code is based on the webgl-noise GLSL shader. For further details
+// of the original shader, please see the following description from the
+// original source code.
+//
+
+//
+// Description : Array and textureless GLSL 2D/3D/4D simplex
+//               noise functions.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : ijm
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//
+
+using UnityEngine;
+using Unity.Mathematics;
+
+
+class SimplexNoise3d
+{
+
+
+    private static float3 mod289(float3 x)
+    {
+        return x - math.floor(x / 289.0f) * 289.0f;
+    }
+
+
+    private static float4 mod289(float4 x)
+    {
+        return x - math.floor(x / 289.0f) * 289.0f;
+    }
+
+
+    private static float4 permute(float4 x)
+    {
+        return mod289((x * 34.0f + 1.0f) * x);
+    }
+
+
+    private static float4 taylorInvSqrt(float4 r)
+    {
+        return 1.79284291400159f - r * 0.85373472095314f;
+    }
+
+
+    public static float4 snoise(float3 v)
+    {
+        float2 c = new float2(1.0f / 6.0f, 1.0f / 3.0f);
+
+        // First corner
+        float3 i = math.floor(v + math.dot(v, c.yyy));
+        float3 x0 = v - i + math.dot(i, c.xxx);
+
+        // Other corners
+        float3 g = math.step(x0.yzx, x0.xyz);
+        float3 l = 1.0f - g;
+        float3 i1 = math.min(g.xyz, l.zxy);
+        float3 i2 = math.max(g.xyz, l.zxy);
+
+        // x1 = x0 - i1  + 1.0 * C.xxx;
+        // x2 = x0 - i2  + 2.0 * C.xxx;
+        // x3 = x0 - 1.0 + 3.0 * C.xxx;
+        float3 x1 = x0 - i1 + c.xxx;
+        float3 x2 = x0 - i2 + c.yyy;
+        float3 x3 = x0 - 0.5f;
+
+        // Permutations
+        i = mod289(i); // Avoid truncation effects in permutation
+        float4 p =
+          permute(permute(permute(i.z + new float4(0.0f, i1.z, i2.z, 1.0f))
+                                + i.y + new float4(0.0f, i1.y, i2.y, 1.0f))
+                                + i.x + new float4(0.0f, i1.x, i2.x, 1.0f));
+
+        // Gradients: 7x7 points over a square, mapped onto an octahedron.
+        // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+        float4 j = p - 49.0f * math.floor(p / 49.0f);  // mod(p,7*7)
+
+        float4 x_ = math.floor(j / 7.0f);
+        float4 y_ = math.floor(j - 7.0f * x_);  // mod(j,N)
+
+        float4 x = (x_ * 2.0f + 0.5f) / 7.0f - 1.0f;
+        float4 y = (y_ * 2.0f + 0.5f) / 7.0f - 1.0f;
+
+        float4 h = 1.0f - math.abs(x) - math.abs(y);
+
+        float4 b0 = new float4(x.xy, y.xy);
+        float4 b1 = new float4(x.zw, y.zw);
+
+        //float4 s0 = float4(lessThan(b0, 0.0)) * 2.0 - 1.0;
+        //float4 s1 = float4(lessThan(b1, 0.0)) * 2.0 - 1.0;
+        float4 s0 = math.floor(b0) * 2.0f + 1.0f;
+        float4 s1 = math.floor(b1) * 2.0f + 1.0f;
+        float4 sh = -math.step(h, 0.0f);
+
+        float4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+        float4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+
+        float3 g0 = new float3(a0.xy, h.x);
+        float3 g1 = new float3(a0.zw, h.y);
+        float3 g2 = new float3(a1.xy, h.z);
+        float3 g3 = new float3(a1.zw, h.w);
+
+        // Normalize gradients
+        float4 norm = taylorInvSqrt(new float4(math.dot(g0, g0), math.dot(g1, g1), math.dot(g2, g2), math.dot(g3, g3)));
+        g0 *= norm.x;
+        g1 *= norm.y;
+        g2 *= norm.z;
+        g3 *= norm.w;
+
+        // Compute noise and gradient at P
+        float4 m = math.max(0.6f - new float4(math.dot(x0, x0), math.dot(x1, x1), math.dot(x2, x2), math.dot(x3, x3)), 0.0f);
+        float4 m2 = m * m;
+        float4 m3 = m2 * m;
+        float4 m4 = m2 * m2;
+        float3 grad =
+          -6.0f * m3.x * x0 * math.dot(x0, g0) + m4.x * g0 +
+          -6.0f * m3.y * x1 * math.dot(x1, g1) + m4.y * g1 +
+          -6.0f * m3.z * x2 * math.dot(x2, g2) + m4.z * g2 +
+          -6.0f * m3.w * x3 * math.dot(x3, g3) + m4.w * g3;
+        float4 px = new float4(math.dot(x0, g0), math.dot(x1, g1), math.dot(x2, g2), math.dot(x3, g3));
+        return 42.0f * new float4(grad, math.dot(m4, px));
+    }
+}
---
+++ Assets/VectorFieldGenerator.cs
@@ -0,0 +1,238 @@
+﻿using JetBrains.Annotations;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Animations;
+
+
+public class VectorFieldGenerator : MonoBehaviour
+{
+
+
+    [Header("Vector Field params")]
+    [SerializeField] private Vector2Int _size;
+    [SerializeField] private FilterMode _filterMode;
+    [SerializeField] private float _scale;
+    [SerializeField] private bool _isSimplex;
+    [SerializeField] private float _step = 8.0f;
+
+    [Header("Gradient")]
+    [SerializeField] private float _epsilon;
+    [SerializeField] private bool _isDivergenceFree;
+    [SerializeField] private bool _isBoundary;
+    [SerializeField] private Rect _borders;
+
+    [Header("Gizmos")]
+    [SerializeField] private float _vectorScale;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _pointScale;
+
+	[Header("Renderer")]
+    [SerializeField] private Transform _rendererTransform;
+    [SerializeField] private float _speedFactor;
+    [SerializeField] private ForceMode _forceMode;
+	
+
+    private Vector4[] _vectorField;
+    private Vector3 _velocity;
+    private float _speed;
+
+
+    private Texture2D GeneratePerlinTexture(Vector2Int size)
+    {
+        Texture2D texture = new Texture2D(size.x, size.y)
+        {
+            hideFlags = HideFlags.HideAndDontSave,
+            filterMode = _filterMode,
+            wrapMode = TextureWrapMode.Repeat
+        };
+
+        Color[] colors = new Color[texture.width * texture.height];
+        _vectorField = new Vector4[texture.width * texture.height];
+        Color c = new Color();
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            Vector4 noise = new Vector4();
+
+            int x = i % texture.width;
+            int y = i / texture.width;
+            //if (x > texture.width * 0.95f) // TODO: remove
+            //{
+            //    noise = 0;
+            //}
+            //else if (x < texture.width * 0.05f) // TODO: remove
+            //{
+            //    noise = 0;
+            //}
+            //if (y > texture.height * 0.95f) // TODO: remove
+            //{
+            //    noise = 0;
+            //}
+            //else if (y < texture.height * 0.05f) // TODO: remove
+            //{
+            //    noise = 0;
+            //}
+
+            if (_isSimplex)
+            {
+                noise = SimplexNoise3d.snoise(new float3(x * _scale + Time.time * 0.3f, y * _scale, Time.time * 0.6f));
+            }
+            else
+            {
+                //noise = Mathf.PerlinNoise(x * _scale, y * _scale);
+            }
+
+            float color = noise.w * 0.5f + 0.5f;
+            c.r = color;
+            c.g = color;
+            c.b = color;
+
+            colors[colors.Length - 1 - i] = c;
+            _vectorField[i] = noise;
+            if (_isDivergenceFree)
+            {
+                _vectorField[i] = Vector3.Cross(_vectorField[i], Vector3.forward);
+            }
+        }
+        
+        texture.SetPixels(colors);
+        texture.Apply();
+
+        return texture;
+    }
+
+
+    private Vector2[] FindVectorField(IList<Color> colors, float step)
+    {
+        Vector2[] v = new Vector2 [colors.Count / (int)(step)];
+
+        for (int i = 0; i < colors.Count; i += (int)(step))
+        {
+            int x = i % _size.x; // 0
+            int y = i / _size.x; // 3
+        
+            float r = (x + _epsilon) + y * _size.x; // 1 + 3 * 4 = 13
+            float l = (x - _epsilon) + y * _size.x; // -1 + 3 * 4 = 11
+            float u = x + (y + _epsilon) * _size.x; // 16
+            float d = x + (y - _epsilon) * _size.x; // 8
+
+            int index = v.Length - 1 - i / (int)(step);
+            try
+            {
+                v[index] = new Vector2(
+                    colors[(int)Mathf.Min(colors.Count - 1, r)].r - colors[(int)Mathf.Max(0, l)].r,
+                    colors[(int)Mathf.Min(colors.Count - 1, u)].r - colors[(int)Mathf.Max(0, d)].r);
+
+                if (_isDivergenceFree)
+                {
+                    v[index] = Vector3.Cross(v[index], Vector3.forward);
+                }
+            }
+            catch
+            {
+                Debug.Log($"For i = {i} it computes index = {index}");
+            }
+        }
+
+        return v;
+    }
+
+	
+    private void Update()
+    {
+	    _speed = Input.GetAxis("Horizontal") * _speedFactor;
+
+	    Vector3 position = _rendererTransform.position;
+
+	    Vector3 grad = SimplexNoise3d.snoise(new float3(position.x * _scale + Time.time * _offset.x, position.y * _scale + Time.time * _offset.y, 0.0f + Time.time * _offset.z)).xyz;
+	    Vector3 grad1 = Vector3.forward; //SimplexNoise3d.snoise(new float3(position.x * _scale, position.y * _scale, 0.0f) + new float3(15.3f, 13.1f, 17.4f)).xyz;
+
+	    grad = Vector3.Cross( grad, grad1);
+
+		if (_isBoundary)
+		{
+			if (position.x >= _size.x - _borders.x)
+			{
+				grad = Vector3.Lerp(grad, Vector3.zero - position, position.x - (_size.x - _borders.x));
+			}
+			else if (position.x <= -_size.x + _borders.x)
+			{
+				grad = Vector3.Lerp(grad, Vector3.zero - position, -position.x - (_size.x - _borders.x));
+			}
+			if (position.y <= -_size.y + _borders.y)
+			{
+				grad = Vector3.Lerp(grad, Vector3.zero - position, -position.y - (_size.y - _borders.y));
+			}
+			else if (position.y >= _size.y - _borders.y)
+			{
+				grad = Vector3.Lerp(grad, Vector3.zero - position, position.y - (_size.y - _borders.y));
+			}
+		}
+
+		_velocity = grad * _speed;
+	    
+	    _rendererTransform.Translate(_velocity);
+	}
+	
+
+
+    private void OnDrawGizmosSelected()
+    {
+	    int i = 0;
+	    for (float x = -_size.x; x < _size.x; x += _step)
+	    {
+		    for (float y = -_size.y; y < _size.y; y += _step)
+		    {
+				Gizmos.color = Color.red;
+			    if (i++ > 1000000)
+			    {
+				    return;
+			    }
+
+			    Vector3 position = new Vector3(x, y, 0);
+
+			    Vector3 grad = SimplexNoise3d.snoise(new float3(position.x * _scale + Time.time * _offset.x, position.y * _scale + Time.time * _offset.y, 0.0f + Time.time * _offset.z)).xyz;
+			    Vector3 grad1 = Vector3.forward;//SimplexNoise3d.snoise(new float3(x * _scale, y * _scale, 0.0f) + new float3(15.3f, 13.1f, 17.4f)).xyz;
+
+				if (_isDivergenceFree)
+			    {
+				    grad =  Vector3.Cross(grad, grad1);
+				}
+
+				if (_isBoundary)
+				{
+				    if (position.x >= _size.x - _borders.x)
+				    {
+					    Gizmos.color = Color.Lerp(Color.red, Color.yellow, position.x - (_size.x - _borders.x));
+						grad = Vector3.Lerp(grad, Vector3.zero - position, position.x - (_size.x - _borders.x));
+				    }
+					else if (position.x <= -_size.x + _borders.x)
+					{
+					    Gizmos.color = Color.Lerp(Color.red, Color.yellow, -position.x - (_size.x - _borders.x));
+						grad = Vector3.Lerp(grad, Vector3.zero - position, -position.x - (_size.x - _borders.x));
+					}
+					if (position.y <= -_size.y + _borders.y)
+					{
+					    Gizmos.color = Color.Lerp(Color.red, Color.yellow, -position.y - (_size.y - _borders.y));
+						grad = Vector3.Lerp(grad, Vector3.zero - position, -position.y - (_size.y - _borders.y));
+					}
+					else if (position.y >= _size.y - _borders.y)
+					{
+					    Gizmos.color = Color.Lerp(Color.red, Color.yellow, position.y - (_size.y - _borders.y));
+						grad = Vector3.Lerp(grad, Vector3.zero - position, position.y - (_size.y - _borders.y));
+					}
+				}
+
+				Gizmos.DrawRay(position, grad * _vectorScale);
+		    }
+	    }
+
+		Gizmos.color = Color.cyan;
+	    Gizmos.DrawRay(_rendererTransform.position, _velocity * _vectorScale * 50f);
+		//Gizmos.color = Color.blue;
+	    //Gizmos.DrawRay(_rendererTransform.position, _rendererTransform.velocity * _vectorScale * 100);
+	}
+
+
+}
